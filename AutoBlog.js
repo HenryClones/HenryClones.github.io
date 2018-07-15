da3e776283fb9ffc9ae875487fa12de023da7a4e
HenryClones <30604707+HenryClones@users.noreply.github.com>
2018-07-15 17:15:28 -0400

    Create AutoBlog.js, return empty object

---
+++ AutoBlog.js
@@ -0,0 +1,10 @@
+// Anonymous function to not interfere with other scopes
+AutoBlog = (function() {
+    // This is the URL for my Google Sheets blog data (in CSV format)
+    var sheet = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQXC_BfT1IFDiigGd7cY504qnPxCA1Q7ZQ6X_eYKl6If5T4TRZmNctvSO947vVCO8l2qgfw67dbIp_z/pub?output=csv";
+    // Our return value for our anonymous function
+    var obj = {};
+    
+    // Break the anonymity
+    return obj;
+})();
