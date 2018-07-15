9ec9b5e39a0a2857c36305691f0efb3c08ce27c4
HenryClones <30604707+HenryClones@users.noreply.github.com>
2018-07-15 17:36:55 -0400

    Update AutoBlog.js

--- AutoBlog.js
+++ AutoBlog.js
@@ -4,7 +4,10 @@ AutoBlog = (function() {
     var sheet = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQXC_BfT1IFDiigGd7cY504qnPxCA1Q7ZQ6X_eYKl6If5T4TRZmNctvSO947vVCO8l2qgfw67dbIp_z/pub?output=csv";
     // Our return value for our anonymous function
     var obj = {};
-    
+    // Regex-based decoder for .CSV files, returns an array
+    obj.decode = function() {
+        
+    };
     // Break the anonymity
     return obj;
 })();
